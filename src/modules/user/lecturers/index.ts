efcc972c08be319cfd72109d32659b4591f9a02a
MANH NGUYEN DUC <manhvttb2004@gmail.com>
2025-03-02 10:06:37 +0700

    update: add index.ts in module students, lecturers

---
+++ src/modules/user/lecturers/index.ts
@@ -0,0 +1,4 @@
+export * from './dto';
+export * from './lecturer.controller';
+export * from './lecturer.module';
+export * from './lecturer.service';
---
+++ src/modules/user/students/index.ts
@@ -0,0 +1,4 @@
+export * from './dto';
+export * from './student.controller';
+export * from './student.module';
+export * from './student.service';
