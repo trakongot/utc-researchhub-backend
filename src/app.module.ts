0e6476bbf9e5e6c25ff1e8e2bd01d61c7385c8c5
MANH NGUYEN DUC <manhvttb2004@gmail.com>
2025-03-02 10:11:19 +0700

    fix: add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { FieldModule } from './modules/academic/field/field.module';
 import { GraduationProjectAllocationsModule } from './modules/enrollment/graduation-project-allocations/graduation-project-allocations.module';
 import { LecturerPreferencesModule } from './modules/enrollment/lecture-preferences';
 import { StudentAdvisingPreferencesModule } from './modules/enrollment/student-advising-preferences/student-advising-preferences.module';
+import { StudentModule } from './modules/user/students';
+import { LecturerModule } from './modules/user/lecturers';
+import { DepartmentModule } from './modules/academic/deparment';
 
 @Module({
   imports: [
@@ -12,6 +15,9 @@ import { StudentAdvisingPreferencesModule } from './modules/enrollment/student-a
     FieldModule,
     GraduationProjectAllocationsModule,
     StudentAdvisingPreferencesModule,
+    StudentModule,
+    LecturerModule,
+    DepartmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
